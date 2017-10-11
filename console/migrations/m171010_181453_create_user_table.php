4fa99ff417626465fe2fab6b3391c004947214b3
michael <michael.karabetsky@gmail.com>
2017-10-11 09:16:18 +0300

    reset

--- console/migrations/m171010_181453_create_user_table.php
+++ console/migrations/m171010_181453_create_user_table.php
@@ -12,21 +12,32 @@ class m171010_181453_create_user_table extends Migration
      */
     public function up()
     {
-        $this->createTable('user', [
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'name' => $this->string(),
-            'email' => $this->string()->defaultValue(null),
-            'password' => $this->string(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),             'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
             'isAdmin' => $this->integer()->defaultValue(0),
             'photo' => $this->string()->defaultValue(null),
-        ]);
+        ], $tableOptions);
     }
+
 
     /**
      * @inheritdoc
      */
     public function down()
     {
-        $this->dropTable('user');
+        $this->dropTable('users');
     }
 }
